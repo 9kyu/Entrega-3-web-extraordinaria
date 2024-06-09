cd50e36841871ee24dc9760565e014268809307d
kyu <luisglezmo@hotmail.com>
2024-06-10 01:14:12 +0200

    avances homepage

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("myNav").style.opacity = "1";
+  document.getElementById("myNav").style.pointerEvents = "auto";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.opacity = "0";
+  document.getElementById("myNav").style.pointerEvents = "none";
+}
